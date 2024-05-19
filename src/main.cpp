f81eaac38e151b82cd69744718eb109a7d450278
pepticthrone291 <intercorers@gmail.com>
2024-05-20 07:49:15 +0900

    first commit first window

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+#include <spdlog/spdlog.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void processInput(GLFWwindow *window);
+
+int main(void)
+{
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit())
+    {
+        const char *description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(800, 600, "2D Engine", nullptr, nullptr);
+    if (!window)
+    {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version: {}", (const char*)glVersion);
+
+    glViewport(0, 0, 600, 400);
+
+    // main loop
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.2f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        processInput(window);
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
